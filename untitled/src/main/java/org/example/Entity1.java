b64394301dc81cee5320423176f70293fdbbd7a2
Noor-Mustafa123 <mustafanoor715@gmail.com>
2024-03-04 20:24:08 +0500

    Configured hibernate and made database connectivity and also added maven build tool in the project

---
+++ untitled/src/main/java/org/example/Entity1.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Entity1 {
+@Id
+    protected int id;
+    protected String name;
+    protected int age;
+    protected String Department;
+
+
+    public Entity1(int id, String name, int age, String department) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        Department = department;
+    }
+
+
+
+    public Entity1() {
+
+    }
+
+//    TODO: what does the try and catach block do?
+//    TODO: Why are super keywords used here even this classs does not implement any interface
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
+    }
+}
+
