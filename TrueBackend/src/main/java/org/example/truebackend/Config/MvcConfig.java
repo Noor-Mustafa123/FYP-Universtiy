d9f4f0bccd1a173644039490958195020b253632
Noor-Mustafa123 <mustafanoor715@gmail.com>
2024-04-28 02:09:48 +0500

    fixed some bugs to deploy the porject on render

--- TrueBackend/src/main/java/org/example/truebackend/Config/MvcConfig.java
+++ TrueBackend/src/main/java/org/example/truebackend/Config/MvcConfig.java
@@ -10,6 +10,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("../../../../../");
+                .addResourceLocations("file:FYP-Universtiy/FYP Project");
     }
 }
