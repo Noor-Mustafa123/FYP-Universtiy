e48597ec86ce082471409ad8684d028c1e37eae2
Noor Mustafa <mustafanoor715@gmail.com>
2024-08-24 22:48:33 +0500

    allowed access for the webhook event to bypass authentication checks

--- TrueBackend/src/main/java/org/example/truebackend/Controllers/ControllerLayer.java
+++ TrueBackend/src/main/java/org/example/truebackend/Controllers/ControllerLayer.java
@@ -53,7 +53,7 @@ public class ControllerLayer {
     @Autowired
     private OrderService orderService;
 
-    private static final String signingSecret = "whsec_6B7trDNjC8hcBwCxEPoghEKGA0Aaytla";
+    private static final String signingSecret = "whsec_9DTptCHInYeSWXk0mouPTqhtrk0yS6mx";
 
 /////////////////////////////////////////////////////////
 //    FOR BETTER ERROR LOGGING AND MORE ROBUST IMPORT A LIBRARY
